469cdafe0ef41011885bab9b39f021965190cf65
pps-admin <pps.github@gmail.com>
2026-03-31 19:33:49 +0900

    Update: path 'generator/generator-raw.cpp' - Updated by 'Dasyma'

--- generator/generator-raw.cpp
+++ generator/generator-raw.cpp
@@ -7,6 +7,11 @@ void case_1()
   std::cout << "5 3\n1 4 1\n1 6 10\n2 3 100\n2 5 1000\n3 5 10000\n2 3\n1 5\n3 4";
 }
 
+void case_2()
+{
+  std::cout << "1 4\n3 5 1\n1 2\n1 3\n5 7\n6 10";
+}
+
 int main(int argc, char** argv)
 {
   registerGen(argc, argv, 1);
@@ -15,6 +20,10 @@ int main(int argc, char** argv)
   if (num == 1)
   {
     case_1();
+  }
+  else if (num == 2)
+  {
+    case_2();
   }
 
   return 0;
